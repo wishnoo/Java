09031bde321486a713d1e693cc35528c02bbc3b8
Wishnoo <rwishnoo@gmail.com>
2022-07-01 04:33:16 -0400

    TopKFrequentElements2 list edit

--- ProblemSolving/Miscellaneous/TopKFrequentElements/TopKFrequentElements2.java
+++ ProblemSolving/Miscellaneous/TopKFrequentElements/TopKFrequentElements2.java
@@ -9,17 +9,26 @@ import java.util.ListIterator;
 public class TopKFrequentElements2 {
     // Implement a 0(N) solution with Array of Arraylist
     public int[] topKFrequent(int[] nums, int k){
+        if(k == nums.length) return nums;
         HashMap<Integer, Integer> processMap = new HashMap<>();
         // We can use ArrayList of ArrayList or Array of ArrayList
         // We cannot use array of ArrayList without warning of unchecked conversion during declaration.
         // Here we are using ArrayList of ArrayList
-        ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>(k);
+        ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+
         // Output to be returned as an array
         int[] output = new int[k];
 
         // Fill hashmap with key and its occurrences
         for (int i = 0; i < nums.length; i++) {
             processMap.put(nums[i], processMap.getOrDefault(nums[i], 0) + 1);
+        }
+
+        // Initialize each arraylist with the main arraylist. This is required as we cannot refer to a particular index as by default list will be of size 0.
+        // Adding a number while initializing list doesn't change as that value means the number of elements it can have before it increase the size.
+        // num.length + 1 is used because one number can be duplicated across the entire input.
+        for (int i = 0; i < nums.length + 1; ++i){
+            list.add(new ArrayList<Integer>());
         }
 
         for(Integer e: processMap.keySet()){
@@ -46,15 +55,19 @@ public class TopKFrequentElements2 {
             while(iterator_sub.hasNext()){
                 output[y++] = iterator_sub.next(); 
             }
+            if(y >= k) break;
         }
 
         return output;
     }
     
     public static void main(String[] args) {
-        TopKFrequentElements obj = new TopKFrequentElements();
+        TopKFrequentElements2 obj = new TopKFrequentElements2();
         int[] nums = {1,1,1,2,2,3};
+        int[] nums2 = {1};
         int k = 2;
+        int k2 = 1;
         System.out.println(Arrays.toString(obj.topKFrequent(nums, k)));
+        System.out.println(Arrays.toString(obj.topKFrequent(nums2, k2)));
     }
 }
