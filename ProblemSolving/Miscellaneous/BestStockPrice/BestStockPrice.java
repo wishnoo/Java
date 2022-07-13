ff8e6c30f16c46f29c369af960ea49487a72d179
Wishnoo <rwishnoo@gmail.com>
2022-07-13 13:55:01 -0400

    BestStockPrice

---
+++ ProblemSolving/Miscellaneous/BestStockPrice/BestStockPrice.java
@@ -0,0 +1,37 @@
+package ProblemSolving.Miscellaneous.BestStockPrice;
+
+public class BestStockPrice {
+    
+    // This question can be solve using Kadane's algorithm. 
+    // current sum saves the difference value and is used for the next iteration. Initial value is 0
+    // From the given example [7,1,5,3,6,4] 
+    // 1-7 = -6 this indicates the stock has reduced and we don't care about 7 anymore we would rather buy at 1 and sale at a latter point
+    // How to retrieve the difference two far apart values:
+    // 5-1 = 4 so 4 is stored in current sum
+    // 3 - 5 = -2 we add -2 with current sum which is 4 and we get 2 which is the difference between 3 and 1. So the overall operation we did combined with previous line is 5 - 1 + 3 - 5
+    // Notice how the 5 cancels each other out. This is how we carry over our difference ahead
+    // 6-3 = 3 adding current sum which is 2 we get 5
+    // at each iteration we update max sum and this allows us to store the max difference.
+    // whenever the value is less than 0 we can reset current sum to 0 which means we starting from scratch for that iteration.
+
+    public int solution(int[] prices) {
+        
+        int currentProfit = 0;
+        int maxProfit = 0;
+        // iterate from 0 to length - 1 to avoid index out of bound exception. 
+        for (int i = 0; i < prices.length -1; i++) {
+            int temp = (prices[i+1] - prices[i]) + currentProfit;
+            currentProfit = Math.max(temp, 0);
+            maxProfit = Math.max(currentProfit, maxProfit);
+        }
+
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        BestStockPrice obj = new BestStockPrice();
+        // int[] prices = {7,1,5,3,6,4};
+        int[] prices = {7,6,4,3,1};
+        System.out.println(obj.solution(prices));
+    }
+}
---
+++ ProblemSolving/Miscellaneous/BestStockPrice/BestStockPrice2.java
@@ -0,0 +1,34 @@
+package ProblemSolving.Miscellaneous.BestStockPrice;
+
+public class BestStockPrice2 {
+    
+    // This solution utilized two pointers
+    public int solution(int[] prices) { 
+        // i saves the lowest value 
+        // j is utilized to iterate through the array.
+        int i = 0, j = 1;
+        int max = 0;
+
+        // iterating though the array
+        while (j < prices.length){
+            // If there is a value less than the value at i reset i to j.
+            if(prices[j] < prices[i]){
+                i = j;
+            }
+            // else you have some difference and update max.
+            else{
+                max = Math.max(max, prices[j] - prices[i]);
+            }
+            j++;
+        }
+        // return int max.
+        return max;
+    }
+
+    public static void main(String[] args) {
+        BestStockPrice obj = new BestStockPrice();
+        int[] prices = {7,1,5,3,6,4};
+        // int[] prices = {7,6,4,3,1};
+        System.out.println(obj.solution(prices));
+    }
+}
