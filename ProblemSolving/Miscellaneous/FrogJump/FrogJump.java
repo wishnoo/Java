4c6c2a3a2137262b75ebe504ef97e4fa8628a45e
Wishnoo <rwishnoo@gmail.com>
2022-07-13 13:54:40 -0400

    FrogJump comments added

--- ProblemSolving/Miscellaneous/FrogJump/FrogJump.java
+++ ProblemSolving/Miscellaneous/FrogJump/FrogJump.java
@@ -8,6 +8,9 @@ public class FrogJump {
     public int solution(int X, int Y, int D){
 
         int distanceRemaining = Y - X;
+        // Math.ceil to get the value equal to more than the distance remaining
+        // type cast of double is required to restrict java which does a floor division by default on integer numbers.
+        // type cast of int is required as the return type is int.
         return (int)Math.ceil((double)distanceRemaining / D);
     }
 
