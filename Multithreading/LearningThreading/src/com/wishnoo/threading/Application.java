73b9d504c69e7fc12c3e6dba67c071b868f522db
Wishnoo <rwishnoo@gmail.com>
2022-08-13 15:30:58 -0400

    Runnable - Anonymous Inner Class

--- Multithreading/LearningThreading/src/com/wishnoo/threading/Application.java
+++ Multithreading/LearningThreading/src/com/wishnoo/threading/Application.java
@@ -5,11 +5,41 @@ public class Application {
 	public static void main(String[] args) {
 		
 		System.out.println("Starting Thread 1");
-		Thread t1 = new Thread(new Task("Thread-A"));
+		Thread t1 = new Thread(new Runnable()  {
+
+			@Override
+			public void run() {
+//				Thread.currentThread().setName(name);
+				for(int i=0; i< 1000; i++) {
+					System.out.println("number: " + i + " Name: " + Thread.currentThread().getName());
+					
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
 		t1.start();
 		
 		System.out.println("Starting Thread 2");
-		Thread t2 = new Thread(new Task("Thread-B"));
+		Thread t2 = new Thread(new Runnable()  {
+
+			@Override
+			public void run() {
+//				Thread.currentThread().setName(name);
+				for(int i=0; i< 1000; i++) {
+					System.out.println("number: " + i + " Name: " + Thread.currentThread().getName());
+					
+					try {
+						Thread.sleep(10);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
 		t2.start();
 		
 	}
