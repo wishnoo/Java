6cbc9bd38b1c108354a7dda743500e377c3b8036
Wishnoo <rwishnoo@gmail.com>
2022-08-13 15:03:10 -0400

    Implement Runnable Interface

--- Multithreading/LearningThreading/src/com/wishnoo/threading/Application.java
+++ Multithreading/LearningThreading/src/com/wishnoo/threading/Application.java
@@ -6,16 +6,18 @@ public class Application {
 		
 		System.out.println("Starting Thread 1");
 		Task taskRunner = new Task("Thread-A");
-		taskRunner.start();
+		Thread t1 = new Thread(taskRunner);
+		t1.start();
 		
 		System.out.println("Starting Thread 2");
 		Task taskRunner2 = new Task("Thread-B");
-		taskRunner2.start();
+		Thread t2 = new Thread(taskRunner2);
+		t2.start();
 		
 	}
 }
 
-class Task extends Thread {
+class Task implements Runnable {
 	
 	private String name;
 	
@@ -30,7 +32,7 @@ class Task extends Thread {
 	public void run(){
 		Thread.currentThread().setName(name);
 		for(int i=0; i< 1000; i++) {
-			System.out.println("number: " + i + "Name: " + Thread.currentThread().getName());
+			System.out.println("number: " + i + " Name: " + Thread.currentThread().getName());
 			
 			try {
 				Thread.sleep(10);
