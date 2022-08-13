32fe8758ea9cb21f55f66f7ae33c3ecd1760816f
Wishnoo <rwishnoo@gmail.com>
2022-08-13 14:51:45 -0400

    setName and getName

--- Multithreading/LearningThreading/src/com/wishnoo/threading/Application.java
+++ Multithreading/LearningThreading/src/com/wishnoo/threading/Application.java
@@ -5,11 +5,11 @@ public class Application {
 	public static void main(String[] args) {
 		
 		System.out.println("Starting Thread 1");
-		Task taskRunner = new Task();
+		Task taskRunner = new Task("Thread-A");
 		taskRunner.start();
 		
 		System.out.println("Starting Thread 2");
-		Task taskRunner2 = new Task();
+		Task taskRunner2 = new Task("Thread-B");
 		taskRunner2.start();
 		
 	}
@@ -17,10 +17,20 @@ public class Application {
 
 class Task extends Thread {
 	
+	private String name;
+	
+	public Task() {
+		
+	}
+	public Task(String name) {
+		this.name = name;
+	}
+	
 	@Override
-	public void run() {
+	public void run(){
+		Thread.currentThread().setName(name);
 		for(int i=0; i< 1000; i++) {
-			System.out.println("number: " + i);
+			System.out.println("number: " + i + "Name: " + Thread.currentThread().getName());
 			
 			try {
 				Thread.sleep(10);
